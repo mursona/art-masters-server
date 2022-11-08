6fd8255b357736c3c197c3d2ad5c03f4f9d8c29f
mursona <mursona>
2022-11-08 14:14:20 +0600

    mongodb connect and environment variable set

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
+// middle wares
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('ema john server is running');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xvlwhga.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('artMasters').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
+    res.send('genius car server is running')
 })
 
-app.listen(port, () =>{
-    console.log(`ema john running on: ${port}`)
+app.listen(port, () => {
+    console.log(`Genius Car server running on ${port}`);
 })
